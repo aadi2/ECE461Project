803c610cea093939f5751520dcbb88abc16a7dab
Alisher Sultansikhov <alish@Alishers-MacBook-Pro.local>
2023-09-23 13:54:20 -0400

    Parser Upd 1.1

--- parser.js
+++ parser.js
@@ -1,27 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getInfo = void 0;
+exports.processUrls = exports.getInfo = void 0;
 var fs = require("fs");
+console.log("parser.js is being executed...");
 var getInfo = function (url) {
     // Your logic to handle and process the URL goes here
     console.log("Processing URL: ".concat(url));
 };
 exports.getInfo = getInfo;
-// Check if this module is being run directly (i.e., `node project.js`)
-if (require.main === module) {
-    var filePath = process.argv[2];
-    if (!filePath) {
-        console.error("No file path provided.");
-        process.exit(1);
-    }
-    // Read the file content
-    fs.readFile(filePath, 'utf8', function (err, data) {
-        if (err) {
-            console.error("Error reading the file: ".concat(err));
+var processUrls = function (filePath) {
+    console.log("Attempting to process file: ".concat(filePath));
+    // Check if this module is being run directly
+    if (require.main === module) {
+        var filePath_1 = process.argv[2];
+        if (!filePath_1) {
+            console.error("No file path provided.");
             process.exit(1);
         }
-        // Split lines and process each URL
-        var urls = data.split('\n').filter(function (url) { return url.trim() !== ''; }); // this will filter out any empty lines
-        urls.forEach(function (url) { return (0, exports.getInfo)(url); });
-    });
-}
+        // Read the file content
+        fs.readFile(filePath_1, 'utf8', function (err, data) {
+            if (err) {
+                console.error("Error reading the file: ".concat(err));
+                process.exit(1);
+            }
+            if (!data.trim()) {
+                console.error("File is empty or contains only whitespace.");
+                process.exit(1);
+            }
+            // Split lines and process each URL
+            var urls = data.split('\n').filter(function (url) { return url.trim() !== ''; }); // this will filter out any empty lines
+            if (urls.length === 0) {
+                console.error("No URLs found in the file.");
+                process.exit(1);
+            }
+            urls.forEach(function (url) { return (0, exports.getInfo)(url); });
+        });
+    }
+};
+exports.processUrls = processUrls;
--- parser.ts
+++ parser.ts
@@ -1,13 +1,16 @@
 import * as fs from 'fs';
 
+console.log("parser.js is being executed...");
+
 export const getInfo = (url: string): void => {
     // Your logic to handle and process the URL goes here
     console.log(`Processing URL: ${url}`);
 };
 
 export const processUrls = (filePath: string): void => {
+    console.log(`Attempting to process file: ${filePath}`);
 
-    // Check if this module is being run directly (i.e., `node project.js`)
+    // Check if this module is being run directly
     if (require.main === module) {
         const filePath = process.argv[2];
         if (!filePath) {
@@ -22,8 +25,17 @@ export const processUrls = (filePath: string): void => {
                 process.exit(1);
             }
 
+            if (!data.trim()) {
+                console.error("File is empty or contains only whitespace.");
+                process.exit(1);
+            }
+
             // Split lines and process each URL
             const urls = data.split('\n').filter(url => url.trim() !== ''); // this will filter out any empty lines
+            if (urls.length === 0) {
+                console.error("No URLs found in the file.");
+                process.exit(1);
+            }
             urls.forEach(url => getInfo(url));
         });
     }
