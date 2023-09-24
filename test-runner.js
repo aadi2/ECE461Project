19574e8cc09c490689d6c6bd1fc6d6e701f2d4a5
Aadi Aggarwal <aadiaggarwal@Aadis-MacBook-Pro.local>
2023-09-24 18:31:04 -0400

    updated test

--- test-runner.js
+++ test-runner.js
@@ -2,8 +2,8 @@ const { exec } = require('child_process');
 const { exit } = require('process');
 const fs = require('fs');
 
-// Use Mocha as the test runner for running the tests
-const command = './node_modules/.bin/mocha --reporter json parser-test.js';
+// Run Jest tests
+const command = './node_modules/.bin/jest --json';
 
 exec(command, (error, stdout, stderr) => {
     if (error) {
@@ -13,36 +13,20 @@ exec(command, (error, stdout, stderr) => {
 
     try {
         const report = JSON.parse(stdout);
-        const passedTests = report.stats.passes;
-        const totalTests = report.stats.tests;
+        const passedTests = report.numPassedTests;
+        const totalTests = report.numTotalTests;
 
-        // For code coverage, we'll utilize nyc (which is Istanbul's command-line tool)
-        exec('./node_modules/.bin/nyc report --reporter=json', (err, coverageOutput) => {
-            if (err) {
-                console.error(`Coverage error: ${err}`);
-                exit(1);
-            }
+        console.log(`${passedTests}/${totalTests} test cases passed.`);
 
-            try {
-                const coverageReport = JSON.parse(coverageOutput);
-                const lineCoverage = (coverageReport.total.lines.covered / coverageReport.total.lines.total) * 100;
+        // Write test results to a file
+        fs.writeFileSync('test-results.ndjson', JSON.stringify({
+            TEST_PASSED: passedTests,
+            TEST_TOTAL: totalTests,
+        }) + '\n');
 
-                console.log(`${passedTests}/${totalTests} test cases passed. ${lineCoverage.toFixed(2)}% line coverage achieved.`);
+        // Exit 0 if all tests pass, otherwise exit 1
+        exit(report.numFailedTests > 0 ? 1 : 0);
 
-                // Writing test results to a file
-                fs.writeFileSync('test-results.ndjson', JSON.stringify({
-                    TEST_PASSED: passedTests,
-                    TEST_TOTAL: totalTests,
-                    LINE_COVERAGE: lineCoverage.toFixed(2)
-                }) + '\n');
-
-                // Exit 0 if all tests pass, otherwise exit 1
-                exit(report.stats.failures > 0 ? 1 : 0);
-            } catch (coverageParseError) {
-                console.error(`Failed to parse coverage report: ${coverageParseError}`);
-                exit(1);
-            }
-        });
     } catch (parseError) {
         console.error(`Failed to parse test report: ${parseError}`);
         exit(1);
